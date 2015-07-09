8883765d4e150d8a9a44f0b14e331016b6799ee5
Claire McQuin <claire@getchef.com>
2015-07-09 13:35:27 -0700

    Use Ohai.config

--- lib/ohai/config.rb
+++ lib/ohai/config.rb
@@ -43,7 +43,7 @@ module Ohai
     # otherwise they will get method_missing'd to nil by Mixlib::Config.
     private
     def self.default_hints_path
-      [ platform_specific_path('/etc/chef/ohai/hints') ]
+      [ ChefConfig::Config.platform_specific_path('/etc/chef/ohai/hints') ]
     end
 
     def self.default_plugin_path
